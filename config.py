d491ddc96e24ecc8ecd84406d3251d8d2ac59de4
david <davidkrivko@gmail.com>
2024-10-16 21:15:51 +0200

    fix

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 
-TROUBLE_SHOOTING_DATA = {"heating_time": 120, "heat_amplitude": [120, 160]}
+TROUBLE_SHOOTING_DATA = {"heating_time": 120, "heat_amplitude": [95, 115]}
 
 TELEGRAM_BOT = os.environ.get("TELEGRAM_BOT")
 CHAT_ID = os.environ.get("CHAT_ID")
