71ec52b5cdd18cd73be17a166ad03be51b74e0e6
david <davidkrivko@gmail.com>
2025-01-27 16:31:18 +0100

    change param

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 
-TROUBLE_SHOOTING_DATA = {"heating_time": 20, "heat_amplitude": [95, 115]}
+TROUBLE_SHOOTING_DATA = {"heating_time": 1800, "heat_amplitude": [95, 115]}
 
 TELEGRAM_BOT = os.environ.get("TELEGRAM_BOT")
 CHAT_ID = os.environ.get("CHAT_ID")
